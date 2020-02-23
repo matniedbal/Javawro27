4a363c8518b9684efb2e514e8dbbb3e374a96c78
matned <mat.niedbal@yahoo.com>
2020-02-23 12:57:54 +0100

    no message

--- src/main/java/pl/sda/rafal/zientara/programowanie2/lesson4/ByteFileComparator.java
+++ src/main/java/pl/sda/rafal/zientara/programowanie2/lesson4/ByteFileComparator.java
@@ -6,7 +6,6 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class ByteFileComparator {
-    private final static int BUFFER_SIZE = 1024;
     private final static int BUFFER_SIZE = 2048;
     private char[] buffer1 = new char[BUFFER_SIZE];
     private char[] buffer2 = new char[BUFFER_SIZE];
